e815d978f72b9c9d3a0c75139282937beaec6e68
İbrahim Hilmi Akargül <ibrahim.akargul+atlassian.com@gmail.com>
2023-04-05 09:12:09 +0300

    First commit

---
+++ src/main/java/com/apibinder/tags/dto/TagDto.java
@@ -0,0 +1,13 @@
+package com.apibinder.tags.dto;
+
+import java.math.BigInteger;
+
+public class TagDto {
+
+    BigInteger id;
+    String key;
+    String value;
+    String uid;
+    Integer createdDateUx;
+
+}
